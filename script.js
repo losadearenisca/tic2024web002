4c815511125906740294e827042ea678aa519368
arc2024cl <160016723+losadearenisca@users.noreply.github.com>
2024-02-20 22:06:05 +0100

    Update script.js

--- script.js
+++ script.js
@@ -167,3 +167,9 @@ function restaurarColorFondo() {
 
 
 }
+var map = L.map('mapid').setView([51.505, -0.09], 13);
+
+// Añadir capa de mapa base
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+}).addTo(map);
